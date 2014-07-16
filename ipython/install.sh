65f2aab8027c4e2bb7f1230b550facb4907db1aa
Tim Dettrick <t.dettrick@uq.edu.au>
2014-07-16 17:06:24 +1000

    Using base for both project types.

--- ipython/install.sh
+++ ipython/install.sh
@@ -1,66 +1,43 @@
 #!/bin/bash
 
 PACKAGES=(
-  # Install supervisord
-  supervisor
-  # Install Telnet (using Busybox telnetd)
-  busybox
-  # Install nginx
-  nginx
-  # Install Git and development tools
-  git vim-enhanced nano wget tmux screen bash-completion
-  # Install node.js
-  nodejs npm
+  # Install build dependencies for Python PIP
+  gcc python-devel
   # Install iPython Notebook dependencies
-  python-pip python-zmq python-jinja2 python-pandas scipy
-  # Install EasyDav dependencies
-  python-kid python-flup
-  # Install patching dependencies
-  patch
+  python-pip
+  # Install scipy dependencies
+  blas-devel lapack-devel
 )
 echo ${PACKAGES[@]}
 
-# Install DNF to make this quicker
-yum install -y dnf
-# Update all packages
-dnf upgrade -y
 # Installation....
 dnf install -y ${PACKAGES[@]}
 # Clean-up downloaded packages to save space
 dnf clean all && yum clean all
-# Install tty.js with updated term.js
-git clone https://github.com/soumith/tty.js.git /opt/tty.js && \
-  cd /opt/tty.js && \
-  npm install
-# Install iPython notebook
-pip-python install ipython tornado
+
+PIP_PACKAGES=(
+  # Notebook dependencies
+  tornado
+  pyzmq
+  jinja2
+  # iPython (with notebook)
+  ipython
+  # Readline for usability
+  pyreadline
+  # Useful iPython libraries
+  ipythonblocks
+  numpy
+  pandas
+  scipy
+)
+
 # Install iPython blocks
-pip-python install --upgrade setuptools
-pip-python install ipythonblocks
-# Instal pyreadline for tab completion
-pip-python install pyreadline
-# Install EasyDAV
-cd /opt && \
-  curl http://koti.kapsi.fi/jpa/webdav/easydav-0.4.tar.gz | tar zxvf - && \
-  mv easydav-0.4 easydav && \
-  cd easydav && \
-  patch -p1 < /tmp/easydav_fix-archive-download.patch && \
-  cd -
-# Install zedrem
-cd /usr/local/bin && \
-  curl http://get.zedapp.org | bash && \
-  cd -
+pip install --upgrade setuptools
 
-# Create researcher user for notebook
-/usr/sbin/useradd researcher
-
-# Log directory for easydav & supervisord
-mkdir -p /var/log/{easydav,supervisor}
-chown -R researcher /var/log/easydav
-
-# Set default passwords
-echo 'root:developer' | chpasswd
-passwd -d researcher && passwd -u -f researcher
+for P in ${PIP_PACKAGES[*]}
+do
+  pip install $P || exit 1
+done
 
 # Create iPython profile
 mkdir -p /opt/ipython
@@ -69,6 +46,3 @@ ipython profile create default
 # Install MathJAX, because CDN is HTTP-only
 python -c "from IPython.external.mathjax import install_mathjax; install_mathjax()"
 chown -R researcher /opt/ipython
-
-# Ensure nginx directories have proper ownership
-chown -R nginx /var/lib/nginx
